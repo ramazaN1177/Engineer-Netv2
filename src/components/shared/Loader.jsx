3c44209fd6daa0cabd9e5ce78c02cde66dd3d4ec
Ramazan <130383236+ramazaN1177@users.noreply.github.com>
2025-03-12 03:03:23 +0300

    lots of done

--- src/components/shared/Loader.jsx
+++ src/components/shared/Loader.jsx
@@ -5,7 +5,7 @@ const Loader = () => {
     <div className='flex-center w-full'>
         <img    src='/Assets/icons/loader.svg'
                 alt='loader'
-                width={24}
+                width={25}
                 height={24}
         />
     </div>
